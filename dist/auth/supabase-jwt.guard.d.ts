1f1dc4b650b97fdcc8fde918a4db75b49af45cec
ranaufalmuha <ifal.muha@gmail.com>
2025-10-17 20:42:57 +0700

    updatee

--- dist/auth/supabase-jwt.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-export declare class SupabaseJwtGuard implements CanActivate {
-    private sb;
-    constructor(cfg: ConfigService);
-    canActivate(ctx: ExecutionContext): Promise<boolean>;
-}
--- dist/common/utils/game-draft.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function deepMerge(a: any, b: any): any;
