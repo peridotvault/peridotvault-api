7eabdc28da31f8c6349448c9128c4757aabba4be
ranaufalmuha <ifal.muha@gmail.com>
2025-10-18 11:02:53 +0700

    try to update cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,19 @@ import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      // Tambahkan domain production nanti, misal:
+      // 'https://studio.peridotvault.com',
+    ],
+    credentials: true, // karena pakai cookie
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.use(cookieParser('RAHASIA'));
   await app.listen(process.env.PORT ?? 3000);
 }
